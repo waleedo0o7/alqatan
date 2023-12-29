83feb74587527c93194838ddc067de817be47858
waleedo0o7 <waleedhd1@gmail.com>
2023-12-29 18:25:54 +0200

    finish intro section

--- layout-intro-section.php
+++ layout-intro-section.php
@@ -1,15 +1,60 @@
 <div class="into-section">
 
-    <div class="top-bar">
-        <h1 class="text-center p-3 bg-primary text-capitalize"> top bar </h1>
+    <div class="co-top-bar bg-primary py-2">
+        <div class="container">
+
+            <div class="top-bar-content d-flex justify-content-between align-items-center">
+
+                <div class="start-content">
+                    <p class="date m-0"> السبت, 29 أبريل 2023 - 9 شوال 1444 هـ </p>
+                </div>
+
+                <div class="end-content">
+                    <p class="date m-0"> <i class="fa fa-search"></i> </p>
+                </div>
+
+            </div>
+
+        </div>
     </div> <!-- top-bar -->
 
-    <div class="hero-image">
-        <h1 class="text-center p-3 bg-secondary color-white text-capitalize"> hero image </h1>
+    <div class="co-intro bg-secondary">
+        <div class="container">
+            <div class="co-hero-image">
+                <img src="assets/images/alqatan-intro.png" class="img-fluid hero-image">
+            </div>
+        </div> <!-- container -->
     </div> <!-- hero-image -->
 
-    <div class="nav-menu">
-        <h1 class="text-center p-3 bg-primary text-capitalize"> nav menu </h1>
+    <div class="co-nav-menu">
+
+        <div class="container">
+
+            <ul class="nav-menu">
+
+                <li class="menu-item"> <a href="#" class="menu-link active"> الرئيسية </a></li>
+
+                <li class="menu-item"> <a href="#" class="menu-link"> السيرة الذاتية </a></li>
+
+                <li class="menu-item"> <a href="#" class="menu-link"> مؤلفات الشيخ </a></li>
+
+                <li class="menu-item"> <a href="#" class="menu-link"> الخطب المنبرية </a></li>
+
+                <li class="menu-item"> <a href="#" class="menu-link"> المحاضرات </a></li>
+
+                <li class="menu-item"> <a href="#" class="menu-link"> مرئيات </a></li>
+
+                <li class="menu-item"> <a href="#" class="menu-link"> لقاءات إذاعية </a></li>
+
+                <li class="menu-item"> <a href="#" class="menu-link"> منبر الأقصى </a></li>
+
+                <li class="menu-item"> <a href="#" class="menu-link"> شروح علمية </a></li>
+
+                <li class="menu-item"> <a href="#" class="menu-link"> كويت الخير </a></li>
+
+            </ul>
+
+        </div>
     </div> <!-- nav-menu -->
 
 </div><!-- into-section -->
